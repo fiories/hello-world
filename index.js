90a20efb572fd60e5bb92c57b9c32c18a0c8c8f3
fiories <laurent33210@yahoo.com>
2023-11-30 12:54:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const app=require('express')();
 const myserveur=require('http').createServer(app);
 const io =require('socket.io')(myserveur);
 app.get('/',(req,res)=>{res.sendFile(`${__dirname}/public/index.html`)});
-io.on('connection',()=>consol.log('un user connecter'));
+io.on('connection',(socket)=>{
+	console.log('un user connecter');
+	//socket.on('chat message',msg)=>{
+	//	consol.log('message :  '+msg);
+		io.emit('chat message',"merrde");
+	//}
+	
+
+});
+
 myserveur.listen(3000,()=>{console.log('Ecoute le port 3000');});
