701a2bd8a6e22f3feb04954bdfd276b3214e6e74
fiories <laurent33210@yahoo.com>
2023-11-30 12:17:02 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http=require('http');
-const myserveur = http.createServer();
-myserveur.on("request",(req,res)=>res.end("ok 2023"));
-myserveur.on("listening",()=>{
-	console.log('listening '+myserveur.address().port + myserveur.address().address);
-	});
-myserveur.listen(8080);
-
